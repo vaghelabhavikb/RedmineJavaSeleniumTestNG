eef60e4904bfe8d57ef6300426a1bff2b35e880a
vaghelabhavikb1 <vaghelabhavikb1@gmail.com>
2026-02-27 15:34:41 +0530

    CommitFrom1-ConflictTry1

--- src/test/java/gitworkflows/CodingBat.java
+++ src/test/java/gitworkflows/CodingBat.java
@@ -3,48 +3,8 @@ package gitworkflows;
 public class CodingBat {
 
 	public static void main(String[] args) {
-		System.out.println(CodingBat.gHappy("xxggyygxx"));
-	}
-
-	public static boolean gHappy(String str) {
-
-		char[] arr = str.toCharArray();
-		int len = arr.length;
-
-		if (len == 1)
-			return false;
-
-		if(len ==0)
-			return true;
-		
-		int gCount = 0;
-		boolean countStarted = false;
-
-		// xxggyygxx
-		for (int i = 0; i < len; i++) {
-			switch (arr[i]) {
-				case 'g':
-					if (!countStarted) {
-						countStarted = true;
-					}
-					gCount++;
-					break;
-				default:
-					if (countStarted && gCount == 1) {
-						return false;
-					} else {
-						countStarted = false;
-						gCount = 0;
-					}
-					break;
-			}
-		}
-		if (gCount == 1) {
-			return false;
-		} else {
-			return true;
-		}
-
+		int i = 1;
+		int j = 1;
 	}
 
 }
